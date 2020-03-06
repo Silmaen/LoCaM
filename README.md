cd795a3db70d669673c644af0a5caaf574055357
Silmaen <genteur.slayer@laposte.net>
2020-03-06 10:07:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# LegendOfCodeAndMagic
+
+Developed with Unreal Engine 4
+
+This is an arena for the LoCaM game
---
+++ Source/LegendOfCodeAndMagic/LegendOfCodeAndMagicGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LegendOfCodeAndMagicGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEGENDOFCODEANDMAGIC_API ALegendOfCodeAndMagicGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
